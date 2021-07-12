de169bc20fb2b26acd2b5a32713a51c5b2f25894
youssef abbas <joo_abs99@icloud.com>
2021-07-12 07:09:43 +0200

    production

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render 
 from django.shortcuts import get_object_or_404
 # Create your views here.
-from .prediction.utilts import segmentation, filter_data, predict_model_class, scale_data, read_signal, plot_ecg_results
+# from .prediction.utilts import segmentation, filter_data, predict_model_class, scale_data, read_signal, plot_ecg_results
 from .models import Patient,ECGFile
 from django.views import View
 
@@ -22,24 +22,24 @@ class apply(View):
         )
         patient.save()
 
-        signal = read_signal(patient.ecg_signal)
-        filter = filter_data(data=signal)
-        segment = segmentation(data=filter[0])
-        results = predict_model_class(data=segment)
+        # signal = read_signal(patient.ecg_signal)
+        # filter = filter_data(data=signal)
+        # segment = segmentation(data=filter[0])
+        # results = predict_model_class(data=segment)
 
 
-        for i in range(len(segment)):
-            plot_ecg_results(segment[i], results[i], patient.id)
-            image_path = f'./media/signal_class/signal{patient.id}{results[i]}.png'
-            ecg = ECGFile(types=results[i],
-                          patient=patient,
-                          signal=image_path)
-            ecg.save()
-        ecg_result = ECGFile.objects.filter(patient=patient)
+        # for i in range(len(segment)):
+        #     plot_ecg_results(segment[i], results[i], patient.id)
+        #     image_path = f'./media/signal_class/signal{patient.id}{results[i]}.png'
+        #     ecg = ECGFile(types=results[i],
+        #                   patient=patient,
+        #                   signal=image_path)
+        #     ecg.save()
+        # ecg_result = ECGFile.objects.filter(patient=patient)
         context = {
-            'ecg_result': ecg_result
+            'patient': patient
         }
-        return render(request,'result.html',context=context)
+        return render(request, 'index.html', context=context)
 
 
 def home(request):
